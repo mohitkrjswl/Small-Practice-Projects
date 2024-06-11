30556f3564b6c6a765924f434664c3e77e5d98e6
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-06-11 22:00:19 +0530

    Practice frontend backend

--- ToDoApp/backend/index.js
+++ ToDoApp/backend/index.js
@@ -1,16 +1,55 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
-
+const cors = require('cors');
+const { todo } = require('./db');
 app.use(express.json());
+app.use(cors());
 
-app.post('/todo', function (req, res) {
-
+app.post('/todo', async function (req, res) {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'You sent the wrong inputs'
+    });
+    return;
+  }
+  //put it in mongodb
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false
+  })
+  res.json({
+    msg: 'Todo created successfully'
+  })
 })
 
-app.get('/todo', function (req, res) {
-
+app.get('/todos', async function (req, res) {
+  const todos = await todo.find({})
+  res.json({
+    todos
+  })
 })
 
-app.post('/completed', function (req, res) {
-
+app.post('/completed', async function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: 'You sent the wrong inputs'
+    })
+    return;
+  }
+  await todo.update({
+    _id: req.body.id
+  }, {
+    completed: true
+  })
+  res.json({
+    msg: 'Todo marked as completed'
+  })
 })
+
+app.listen(3000);
